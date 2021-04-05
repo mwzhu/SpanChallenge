ecc73c796ff64e38ed9cf5f99798bac93aa2ed15
Michael Zhu <michaelwzhu@berkeley.edu>
2021-04-05 06:04:44 -0700

    finish

--- setup.py
+++ setup.py
@@ -4,13 +4,12 @@ with open("README.md", 'r') as f:
     long_description = f.read()
 
 setup(
-    name = 'JSON Converter',
+    name = 'Span Challenge',
     version = '1.0',
     description = 'A module to convert JSON files containing notifications from an old format to a new format which also removes duplicates and sorts notifications',
     license = 'MIT',
     long_description = long_description,
     author = 'Michael Zhu',
     author_email = 'michaelwzhu@berkeley.edu',
-    packages = ['JSON Converter'],
-    install_requires = ['dateutil']
+    install_requires = ['python-dateutil']
 )
