56518b95b30b0b70a5f244ecb5e1d1125058d142
Michael Zhu <michaelwzhu@berkeley.edu>
2021-04-05 05:33:36 -0700

    final

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ with open("README", 'r') as f:
     long_description = f.read()
 
 setup(
-    name = 'JSON Converter'
-    version = '1.0'
-    description = 'A module to convert JSON files containing notifications from an old format to a new format which also removes duplicates and sorts notifications'
-    license = 'MIT'
-    long_description = long_description
-    author = 'Michael Zhu'
-    author_email = 'michaelwzhu@berkeley.edu'
-    packages = ['JSON Converter']
+    name = 'JSON Converter',
+    version = '1.0',
+    description = 'A module to convert JSON files containing notifications from an old format to a new format which also removes duplicates and sorts notifications',
+    license = 'MIT',
+    long_description = long_description,
+    author = 'Michael Zhu',
+    author_email = 'michaelwzhu@berkeley.edu',
+    packages = ['JSON Converter'],
+    install_requires = ['dateutil']
 )
